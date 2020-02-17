e81759f1bf2fe38afe681472f2ccd1d033860583
Jesse Bartola <jrbartola@gmail.com>
2020-02-17 14:19:51 -0500

    Add cli options finder

---
+++ options_finder.py
@@ -0,0 +1,37 @@
+import argparse
+
+from trades.iron_condor_finder import find_profitable_iron_condors
+from trades.vertical_finder import find_profitable_verticals
+
+from settings import update_df_display_settings
+from constants.strategies import IRON_CONDOR, CREDIT_VERTICAL
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Find options trades.')
+    parser.add_argument('--symbol', help='The (optionable) underlying symbol', required=True)
+    parser.add_argument('--strategy', help='The strategy to search for (see docs)', required=True)
+    parser.add_argument('--contract', help='The type of contract to search for (CALL or PUT)', required=False)
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    # Needed so that the dataframes show all row/column values
+    update_df_display_settings()
+
+    args = get_arguments()
+    symbol = args.symbol
+    
+    if args.strategy == IRON_CONDOR:
+        find_profitable_iron_condors(symbol)
+    
+    elif args.strategy == CREDIT_VERTICAL:
+        if not args.contract:
+            raise AttributeError('Argument `--contract` must be supplied with the CREDIT_VERTICAL strategy')
+
+        if args.contract not in {'CALL', 'PUT'}:
+            raise ValueError(f'Argument `--contract` must be either `CALL` or `PUT`. Got `{args.contract}`')
+
+        find_profitable_verticals(symbol, args.contract)
+
+    else:
+        raise ValueError(f'Invalid strategy `{args.strategy}`. See docs for possible values')
