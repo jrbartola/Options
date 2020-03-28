68fe87beb754343ebc348b2e3c7152d9a207a46a
Jesse Bartola <jrbartola@gmail.com>
2020-03-27 20:29:58 -0400

    Rename volatility --> historical volatility

--- api/quotes.py
+++ api/quotes.py
@@ -6,7 +6,7 @@ from api.client import c
 def get_price_quote(symbol):
     return c.quote(symbol)[symbol.upper()]
 
-def get_volatility(symbol):
+def get_historical_volatility(symbol):
     candles = c.history(symbol, periodType='year', period=1, frequencyType='daily')['candles']
 
     price_comps = [candles[i]['close'] / candles[i-1]['close'] - 1 for i in range(1, len(candles))]
